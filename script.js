95f669da8d0b366c0dd93d17b24925aafe7f52a3
Daramola Kehinde <daramolakenny18@gmail.com>
2024-09-18 04:02:32 +0100

    dice game

---
+++ script.js
@@ -0,0 +1,9 @@
+randomNum = Math.floor(Math.random(1) * 6 + 1);
+
+document.getElementById("dice1").setAttribute("src", "./image/dice5.png");
+document.getElementById("dice2").setAttribute("src", "./image/dice3.png");
+
+// randomNum = Math.floor(Math.random(1) * 6 + 1);
+
+// document.getElementById("dice1").setAttribute("src", "./image/dice5.png");
+// document.getElementById("dice2").setAttribute("src", "./image/dice3.png");
